7497be05cf8b18f31bb3bf8d8016592fb0f09354
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-06-03 19:02:35 +0900

    설정

--- spring3/src/test/java/com/koreait/ex01/Ex01.java
+++ spring3/src/test/java/com/koreait/ex01/Ex01.java
@@ -1,12 +1,30 @@
 package com.koreait.ex01;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 public class Ex01 {
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void test1() throws SQLException {
+        Connection con = dataSource.getConnection();
+        System.out.println(con);
 
 
+
+
+
+
+    }
 
 
 
