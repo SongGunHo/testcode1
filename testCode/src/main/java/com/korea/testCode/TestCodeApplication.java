ea9dc11fe67d4f7554677f27bc43232c1aa72088
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-06-18 23:49:16 +0900

    설정 중

--- testCode/src/main/java/com/korea/testCode/TestCodeApplication.java
+++ testCode/src/main/java/com/korea/testCode/TestCodeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestCodeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestCodeApplication.class, args);
 	}
 
